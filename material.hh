fcd2772e73fa9a82f30d44664c68d5bbdcf95737
Osamu Koga <osak.63@gmail.com>
2021-02-13 02:17:14 -0600

    Rough metals

--- material.hh
+++ material.hh
@@ -40,16 +40,17 @@ class lambertian : public material {
 
 class metal : public material {
     public:
-        metal(const color& a) : albedo(a) {}
+        metal(const color& a, double f) : albedo(a), fuzz(f) {}
 
         virtual bool scatter(
             const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered
         ) const override {
             vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected);
+            scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
             attenuation = albedo;
             return dot(scattered.direction(), rec.normal) > 0;
         }
     private:
         color albedo;
+        double fuzz;
 };
