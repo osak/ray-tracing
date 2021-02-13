987339e55dd27bfd622ecf7ca122c059f93a8953
Osamu Koga <osak.63@gmail.com>
2021-02-13 04:18:32 -0600

    final

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
-SRC=main.cc $(wildcard *.hh)
-main: $(SRC)
+HEADERS=$(wildcard *.hh)
+main: main.cc $(HEADERS)
 	g++ -o$@ -O3 main.cc
+
+final: final.cc $(HEADERS)
+	g++ -o$@ -O3 final.cc
---
+++ final.cc
@@ -0,0 +1,134 @@
+#include "rtweekend.hh"
+
+#include "color.hh"
+#include "vec3.hh"
+#include "ray.hh"
+#include "hittable_list.hh"
+#include "sphere.hh"
+#include "camera.hh"
+#include "material.hh"
+
+#include <iostream>
+
+hittable_list random_scene() {
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+
+    for (int a = -11; a < 11; ++a) {
+        for (int b = -1; b < 11; ++b) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() <= 0.9) {
+                continue;
+            }
+
+            if (choose_mat < 0.8) {
+                auto albedo = color::random() * color::random();
+                auto sphere_material = make_shared<lambertian>(albedo);
+                world.add(make_shared<sphere>(center, 0.2, sphere_material));
+            } else if (choose_mat < 0.95) {
+                auto albedo = color::random(0.5, 1);
+                auto fuzz = random_double(0, 0.5);
+                auto sphere_material = make_shared<metal>(albedo, fuzz);
+                world.add(make_shared<sphere>(center, 0.2, sphere_material));
+            } else {
+                auto sphere_material = make_shared<dielectric>(1.5);
+                world.add(make_shared<sphere>(center, 0.2, sphere_material));
+            }
+        }
+    }
+
+    // The glass ball
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    // The ball
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
+color ray_color(const ray& r, const hittable& world, int depth) {
+    hit_record rec;
+
+    // The ray can't bounce anymore. It's dissolved into the darkness...
+    if (depth <= 0) {
+        return color(0, 0, 0);
+    }
+
+    // Reflection point of some rays are not exactly on the surface, but some rays reflect off of
+    // slightly inner point of sphere due to floting point error.
+    // To workaround this error, ignore the hits that are too close at the origin.
+    if (world.hit(r, 0.001, infinity, rec)) {
+        ray scattered;
+        color attenuation;
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+            return attenuation * ray_color(scattered, world, depth-1);
+        } else {
+            return color(0, 0, 0);
+        }
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+
+    // Normalize y component of range [-1.0, 1.0] into [0.0, 1.0]
+    auto level = 0.5 * (unit_direction.y() + 1.0);
+
+    // (roughly) white in the bottom, sky-blue on the top
+    return (1.0-level) * color(1.0, 1.0, 1.0) + level*color(0.5, 0.7, 1.0);
+}
+
+
+int main(int argc, char **argv) {
+    int seed = std::stoi(std::string(argv[1]));
+    int from_scanline = std::stoi(std::string(argv[2]));
+    int to_scanline = std::stoi(std::string(argv[3]));
+
+    srand(seed);
+
+    // Canvas settings
+    const auto aspect_ratio = 3.0 / 2.0;
+    const int image_width = 1200;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 500;
+    const int max_depth = 50;
+
+    // World settings
+    hittable_list world = random_scene();
+
+    // Camera settings
+    point3 look_from(13, 2, 3);
+    point3 look_at(0, 0, 0);
+    vec3 vup(0, 1, 0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
+    //shared_ptr<camera> cam = make_shared<ideal_camera>(look_from, look_at, vup, 20, aspect_ratio);
+    shared_ptr<camera> cam = make_shared<lens_camera>(look_from, look_at, vup, 20, aspect_ratio, aperture, dist_to_focus);
+
+    // Render
+    std::cout << "P3\n" << image_width << ' ' << image_height << std::endl;
+    std::cout << 255 << std::endl;
+
+    for (int j = from_scanline; j >= to_scanline; --j) {
+        std::cerr << "\rScanlines remaining: " << j << "   " << std::flush;
+
+        for (int i = 0; i < image_width; ++i) {
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = double(i + random_double()) / (image_width-1);
+                auto v = double(j + random_double()) / (image_height-1);
+                ray r = cam->get_ray(u, v);
+                pixel_color += ray_color(r, world, max_depth);
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel);
+        }
+    }
+    std::cerr << std::endl << "Done" << std::endl;
+}
